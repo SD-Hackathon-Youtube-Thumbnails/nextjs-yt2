1453371793bc9c111c74aef7390abdcfcbe6d8c5
Tycoon <69978243+tyc0on@users.noreply.github.com>
2023-04-22 23:27:17 +1000

    loading

--- components/BackgroundSelection.js
+++ components/BackgroundSelection.js
@@ -1,11 +1,15 @@
 // components/BackgroundSelection.js
 
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 const BackgroundSelection = ({ bgData }) => {
   const router = useRouter();
+  const [loadingImage, setLoadingImage] = useState(false);
 
   const handleBackgroundSelection = async (imgUrl, text) => {
+    setLoadingImage(true); // Set loading state to true
+
     const response = await fetch(
       `https://datawb.com/fit-text.php?img=${encodeURIComponent(
         imgUrl
@@ -16,11 +20,18 @@ const BackgroundSelection = ({ bgData }) => {
       pathname: "/editor",
       query: { editorData: JSON.stringify(data) },
     });
+
+    setLoadingImage(false); // Set loading state back to false
   };
 
   return (
     <div>
       <h1>Select a background</h1>
+      {loadingImage && (
+        <div>
+          <p>Loading image...</p>
+        </div>
+      )}
       <div style={{ display: "flex", flexWrap: "wrap" }}></div>
       {Object.entries(bgData).map(([key, url]) => {
         if (key.startsWith("bg")) {
